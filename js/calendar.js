613508196ee941bf4eced7478da550a336729fd9
Naoto Ikuno <moon_in_france@yahoo.co.jp>
2016-06-27 15:39:43 +0900

    first commit.

---
+++ js/calendar.js
@@ -0,0 +1,22 @@
+class Calendar {
+    constructor(year, month) {
+        this.weeks = [];
+        const first = new Date(year, month, 1);
+        const last = new Date(year, month + 1, 0);
+        let week = [];
+        for (let i = 1 - first.getDay(); i <= last.getDate(); i = 0 | i + 1) {
+            week[week.length] = i > 0 ? i : '';
+            if (week.length >= 7) {
+                this.weeks[this.weeks.length] = week;
+                week = [];
+            }
+        }
+        if (week.length != 0) {
+            while (week.length < 7) {
+                week[week.length] = '';
+            }
+            this.weeks[this.weeks.length] = week;
+            week = [];
+        }
+    }
+}
